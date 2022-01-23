d9866e19ec720d10cfc51b66dee8da64e1815647
JuanCarlos-CarreteroRoldan <jcarretero@elpuig.xeill.net>
2022-01-23 16:18:02 +0100

    v.1: Dice hola mon y hola

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola mon");
         System.out.println("hola");
-        System.out.println("adios");
+        System.out.println("adioss");
     }
 }
